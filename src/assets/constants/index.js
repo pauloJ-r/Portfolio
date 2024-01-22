6090093ec0c705b2f5893cf359771f7fa2a64b27
pauloJ-r <pj74586@gmail.com>
2024-01-22 12:26:23 -0300

    style:alteração do nome dos links

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -1,6 +1,6 @@
 export let Links =[
-  {id: 1,title:"Home"},
-  {id: 2,title:"About"},
-  {id: 3,title:"Project"},
-  {id: 4,title:"Contact"},
+  {id: 1,title:"Inicio"},
+  {id: 2,title:"Sobre"},
+  {id: 3,title:"Projetos"},
+  {id: 4,title:"Contatos"},
 ];
