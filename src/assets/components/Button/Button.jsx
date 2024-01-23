caf7b51b1d36a6be1040c853e39e38e554767277
pauloJ-r <pj74586@gmail.com>
2024-01-23 20:06:29 -0300

    style: estilizando botão

--- src/assets/components/Button/Button.jsx
+++ src/assets/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <p className='bg-[#E7E7E7] px-4 py-2 font-mulish text-[16px] text-gray-600 font-bold rounded-lg'>
+    <p className='bg-[#E7E7E7] px-4 py-2 font-poppins text-[16px] text-gray-600 font-bold rounded-lg'>
       {props.children}
     </p>
   )
