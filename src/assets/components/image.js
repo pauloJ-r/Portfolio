c98c379fd12762950ff27c071a867f2c98c3bcec
pauloJ-r <pj74586@gmail.com>
2024-01-24 09:01:44 -0300

    feat: construção da aréa de ferramentas

--- src/assets/components/image.js
+++ src/assets/components/image.js
@@ -1,8 +1,16 @@
 import Paulo from "../../Images/paulo.png";
 import Github from "../../Images/logo-github.svg";
 import LinkedIn from "../../Images/logo-linkedin.svg";
+import Figma from "../../Images/logo-figma.svg";
+import Vscode from "../../Images/logo-vscode.svg"
+import Ps from "../../Images/logo-ps.png"
+import Windows from "../../Images/logo-windows.svg"
 export{
     Paulo,
     Github,
     LinkedIn,
+    Figma,
+    Vscode,
+    Ps,
+    Windows,
 };
--- src/assets/sections/About.jsx
+++ src/assets/sections/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '../components/Button/Button'
+import { Figma, Vscode, Ps, Github, Windows } from '../components/image'
 const About = () => {
   return (
     <div id='Sobre Mim'>
@@ -49,6 +50,50 @@ const About = () => {
         </Button>
        </div>
       </div>
+      </div>
+      <div className="py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h1 className='font-poppins pb-6 text-2xl mb-4 text-center'>
+          Ferramentas
+        </h1>
+        <div className='mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5'>
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src={Vscode}
+            alt="Vscode"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src={Github}
+            alt="Github"
+            width={158}
+            height={48}
+          />
+        <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src={Figma}
+            alt="Figma"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+            src={Ps}
+            alt="Photoshop"
+            width={158}
+            height={48}
+          />
+             <img
+            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+            src={Windows}
+            alt="Windows"
+            width={158}
+            height={48}
+          />
+        </div>
+        </div>
       </div>
       </div>
       
